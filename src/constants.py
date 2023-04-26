8295d32c8dddc7ad11956c695dd0e3fde6f7acf4
mahuna13 <jk871@cam.ac.uk>
2023-04-26 22:16:54 +0000

    Adds methods for downloading GEDI data

---
+++ src/constants.py
@@ -0,0 +1,96 @@
+"""Module contains all project wide constants."""
+import logging
+import os
+from pathlib import Path
+from enum import Enum
+
+import dotenv
+
+dotenv.load_dotenv()
+
+
+class GediProduct(Enum):
+    L1B = "level1B"
+    L2A = "level2A"
+    L2B = "level2B"
+    L3 = "level3"
+    L4A = "level4A"
+    L4B = "level4B"
+
+
+# ---------------- PATH CONSTANTS -------------------
+#  Source folder path
+constants_path = Path(__file__)
+SRC_PATH = constants_path.parent
+PROJECT_PATH = SRC_PATH.parent
+CONDA_ENV = os.getenv("CONDA_DEFAULT_ENV")
+
+# Log relatedd paths
+LOG_PATH = PROJECT_PATH / "logs"
+LOG_PATH.mkdir(parents=True, exist_ok=True)
+
+#  Data related paths
+DATA_PATH = Path(os.getenv("DATA_PATH"))
+USER_PATH = Path(os.getenv("USER_PATH"))
+EARTHDATA_USER = os.getenv("EARTHDATA_USER")
+EARTHDATA_PASSWORD = os.getenv("EARTHDATA_PASSWORD")
+
+GEDI_PATH = DATA_PATH / "GEDI"
+
+
+def gedi_product_path(product):
+    return GEDI_PATH / product.value
+
+
+GEDI_L1B_PATH = gedi_product_path(GediProduct.L1B)
+GEDI_L2A_PATH = gedi_product_path(GediProduct.L2A)
+GEDI_L4A_PATH = gedi_product_path(GediProduct.L4A)
+JRC_PATH = DATA_PATH / "JRC"
+ENV_VARS_PATH = DATA_PATH / "EnvVars"
+PLANET_PATH = DATA_PATH / "Planet"
+PAISAGENSLIDAR_PATH = DATA_PATH / "Paisagenslidar"
+EBALIDAR_PATH = DATA_PATH / "EBA_lidar"
+
+ENV_VARS_NAMES = ["defMean", "SCCsoil", "fpar", "lightning", "srtm"]
+
+# ---------------- API KEYS -------------------------
+PLANET_API_KEY = os.getenv("PLANET_API_KEY")
+
+# ---------------- LOGGING CONSTANTS ----------------
+DEFAULT_FORMATTER = logging.Formatter(
+    (
+        "%(asctime)s %(levelname)s: %(message)s "
+        "[in %(funcName)s at %(pathname)s:%(lineno)d]"
+    )
+)
+DEFAULT_LOG_FILE = LOG_PATH / "default_log.log"
+DEFAULT_LOG_LEVEL = logging.INFO  # verbose logging per default
+
+# ---------------- PROJECT CONSTANTS ----------------
+# Coordinate reference systems (crs)
+WGS84 = "EPSG:4326"  # WGS84 standard crs (latitude, longitude)
+WEBMERCATOR = "EPSG:3857"  # CRS for web maps
+SIRGAS_BRAZIL = "EPSG:5880"  # Polyconic projected CRS for Brazil
+
+WGS84_UTM18S = "EPSG:32718"  # https://epsg.io/32718
+WGS84_UTM19S = "EPSG:32719"  # https://epsg.io/32719
+WGS84_UTM20S = "EPSG:32720"  # https://epsg.io/32720
+WGS84_UTM21S = "EPSG:32721"  # https://epsg.io/32721
+WGS84_UTM22S = "EPSG:32722"  # https://epsg.io/32722
+WGS84_UTM23S = "EPSG:32723"  # https://epsg.io/32723
+WGS84_UTM24S = "EPSG:32724"  # https://epsg.io/32724
+
+SIRGAS2000_UTM18S = "EPSG:31978"  # https://epsg.io/31978
+SIRGAS2000_UTM19S = "EPSG:31979"  # https://epsg.io/31979
+SIRGAS2000_UTM20S = "EPSG:31980"  # https://epsg.io/31980
+SIRGAS2000_UTM21S = "EPSG:31981"  # https://epsg.io/31981
+SIRGAS2000_UTM22S = "EPSG:31982"  # https://epsg.io/31982
+SIRGAS2000_UTM23S = "EPSG:31983"  # https://epsg.io/31983
+SIRGAS2000_UTM24S = "EPSG:31984"  # https://epsg.io/31984
+
+# ---------------- DATABASE CONSTANTS ----------------
+DB_HOST = os.getenv("DB_HOST")  # JASMIN database server
+DB_NAME = os.getenv("DB_NAME")  # Database for GEDI shots
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_CONFIG = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
---
+++ src/data/gedi_loader.py
@@ -0,0 +1,61 @@
+import geopandas as gpd
+
+from src import constants
+from src.data.gedi_database import GediDatabase
+from src.utils.logging_util import get_logger
+
+logger = get_logger(__file__)
+
+
+def get_gedi_shots(
+    geometry: gpd.GeoDataFrame,
+    start_year: int,
+    end_year: int,
+    crs: str = constants.WGS84,
+):
+    database = GediDatabase()
+
+    # Load GEDI data within tile
+    logger.info(
+        f'Loading Level 4a GEDI shots for period {start_year}-{end_year} in this geometry')
+    gedi_shots = database.query(
+        table_name="level_4a",
+        columns=[
+            "shot_number",
+            "absolute_time",
+            "lon_lowestmode",
+            "lat_lowestmode",
+            "agbd",
+            "agbd_pi_lower",
+            "agbd_pi_upper",
+            "agbd_se",
+            "l2_quality_flag",
+            "l4_quality_flag",
+            "degrade_flag",
+            "beam_type",
+            "sensitivity",
+            "pft_class"
+        ],
+        geometry=geometry,
+        crs=crs,
+        start_time=f"{start_year}-01-01",
+        end_time=f"{end_year}-01-01",
+    )
+    logger.debug(
+        f'Found {len(gedi_shots)} shots in {start_year}-{end_year} in the specified geometry')
+
+    # Preliminary filtering to reduce computation size
+    gedi_shots = gedi_shots[
+        (gedi_shots.l2_quality_flag == 1)
+        & (gedi_shots.l4_quality_flag == 1)
+        & (gedi_shots.degrade_flag == 0)
+    ]
+    logger.info(f'Number of GEDI shots found: {gedi_shots.shape[0]}')
+    gedi_shots.to_csv('/maps/fire-regen/data/sierras_gedi_shots.csv')
+    return gedi_shots
+
+
+if __name__ == "__main__":
+    # shape_gpd = gpd.read_file("data/shapefiles/seki_convex_hull.shp")
+    shape_gpd = gpd.read_file("data/shapefiles/sierras_convex_hull.shp")
+    get_gedi_shots(shape_gpd.geometry, 2019, 2023)
