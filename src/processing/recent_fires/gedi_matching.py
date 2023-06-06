4a10581573912200ecce9ad8ddfa0ab3d5aaa781
mahuna13 <jk871@cam.ac.uk>
2023-06-06 15:51:02 +0100

    Simplifying pai z logic

--- src/processing/recent_fires/gedi_matching.py
+++ src/processing/recent_fires/gedi_matching.py
@@ -23,20 +23,21 @@ def find_matches(
     '''
     left_input = get_severity(left, severity)
     right_input = get_severity(right, severity)
-    closest_indeces, distances = k_nn.nearest_neighbors(left, right, 1)
+    closest_indeces, distances = k_nn.nearest_neighbors(
+        left_input, right_input, 1)
 
-    result = left.copy()
+    result = left_input.copy()
     result['closest_distance'] = distances
     result[f'match_datetime'] = pd.to_datetime(
-        right.iloc[closest_indeces.flatten()
-                   ].absolute_time.values, utc=True, format='mixed')
+        right_input.iloc[closest_indeces.flatten()
+                         ].absolute_time.values, utc=True, format='mixed')
 
     if column is not None:
         columns = [column]
 
     for col in columns:
-        result[f'{col}_after'] = right.iloc[closest_indeces.flatten()
-                                            ][col].values
+        result[f'{col}_after'] = right_input.iloc[closest_indeces.flatten()
+                                                  ][col].values
         result[f'{col}_diff'] = result[col] - result[f'{col}_after']
         result[f'{col}_rel'] = result[f'{col}_after']/result[col]
 
@@ -85,52 +86,15 @@ def match_pai_z_before_and_after_fire(
     before_fire = pai_vertical.transform_pai_z(before_fire)
     after_fire = pai_vertical.transform_pai_z(after_fire)
 
-    print(before_fire.columns)
-    # columns = [f'pai_z_{i}' for i in range(15)]
-
     # For each shot in before fire, find the closest shot after fire.
-    result = find_matches_pai_z(before_fire, after_fire)
+    # Break it down per severity.
+    result_low = find_matches(before_fire, after_fire, 2, 'pai_z_delta_np')
+    result_medium = find_matches(before_fire, after_fire, 3, 'pai_z_delta_np')
+    result_high = find_matches(before_fire, after_fire, 4, 'pai_z_delta_np')
+
+    result = pd.concat([result_low, result_medium, result_high])
     result['start_offset'] = start_offset
     result['end_offset'] = end_offset
-    return result
-
-
-def find_matches_pai_z(
-        left: gpd.GeoDataFrame,
-        right: gpd.GeoDataFrame,
-        column: str = None,
-        columns: list[str] = None
-) -> gpd.GeoDataFrame:
-    '''
-    For each row in the left GeoDataFrame, it finds the closest row in the 
-    right GeoDataFrame, and extracts column values from the matching row.
-    '''
-    closest_indeces, distances = k_nn.nearest_neighbors(left, right, 1)
-
-    result = left.copy()
-    result['closest_distance'] = distances
-    result[f'match_datetime'] = pd.to_datetime(
-        right.iloc[closest_indeces.flatten()
-                   ].absolute_time.values, utc=True, format='mixed')
-
-    if column is not None:
-        columns = [column]
-
-    print(1)
-    '''for col in columns:
-        result[f'{col}_after'] = right.iloc[closest_indeces.flatten()
-                                            ][col].values
-        result[f'{col}_diff'] = result[col] - result[f'{col}_after']
-        result[f'{col}_rel'] = result[f'{col}_after']/result[col]'''
-
-    result[f'pai_z_delta_after'] = right.iloc[closest_indeces.flatten()
-                                              ]['pai_z_delta_np'].values
-    print(2)
-    result[f'pai_z_delta_diff'] = result['pai_z_delta_np'] - \
-        result[f'pai_z_delta_after']
-    print(3)
-    result[f'pai_z_delta_rel'] = result[f'pai_z_delta_after'] / \
-        result['pai_z_delta_np']
     return result
 
 
