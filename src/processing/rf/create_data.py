c12cd4f498069aa8956e885186440db590193873
mahuna13 <jk871@cam.ac.uk>
2023-06-24 20:42:28 +0100

    Adds land cover filtering to training on unburned data

--- src/processing/rf/create_data.py
+++ src/processing/rf/create_data.py
@@ -7,13 +7,22 @@ from fastai.tabular.all import save_pickle
 logger = get_logger(__file__)
 
 
-def create_and_save_data_for_rf(gedi, start_year, end_year, save: bool = False, save_folder: str = None):
+def create_and_save_data_for_rf(
+        gedi,
+        start_year,
+        end_year,
+        save: bool = False,
+        save_folder: str = None,
+        filter_land_cover: bool = False):
     # Match with 5 years of Landsat data from the past.
     gedi_match = gedi.copy()
     kernel = 3
 
     counter = 0
     for year in range(start_year, end_year):
+        if filter_land_cover:
+            gedi_match = filter_land_cover_for_trees(gedi_match, year)
+
         logger.debug(f'Matching gedi shots from {year}')
         raster = gedi_raster_matching.get_landsat_raster_sampler(year)
 
@@ -40,6 +49,24 @@ def create_and_save_data_for_rf(gedi, start_year, end_year, save: bool = False, 
                 columns=[f"{x}_{year-4}" for x in spectral])
 
     return gedi_match
+
+
+def filter_land_cover_for_trees(
+        gedi,
+        year):
+    gedi_lc = gedi_raster_matching.match_landcover_for_year(
+        year, gedi, 3)
+    # Keep only trees.
+    gedi_lc = gedi_lc[
+        (gedi_lc.land_cover_std == 0) &
+        (gedi_lc.land_cover_median == 1)]
+
+    columns_to_drop = [f"land_cover_3x3",
+                       "land_cover_mean",
+                       "land_cover_std",
+                       "land_cover_median"]
+    gedi_lc.drop(columns=columns_to_drop, inplace=True)
+    return gedi_lc
 
 
 def process_spectral_column_names(df, year, kernel):
