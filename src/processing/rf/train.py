ab36ad9ddb456fdf85892b2b1f1f48d41c2dfe38
mahuna13 <jk871@cam.ac.uk>
2023-12-19 19:11:58 +0000

    Adds code for training RF on rh 98 data

--- src/processing/rf/train.py
+++ src/processing/rf/train.py
@@ -6,6 +6,9 @@ from fastai.tabular.all import *
 from sklearn.metrics import *
 from src.utils.logging_util import get_logger
 from sklearn.ensemble import RandomForestRegressor
+from src.data import shape_processor
+from src.constants import DATA_PATH, USER_PATH
+import geopandas as gpd
 import numpy as np
 
 
@@ -22,6 +25,12 @@ def r_squared(pred, y): return r2_score(y, pred)
 
 
 def m_r2(m, xs, y): return r_squared(m.predict(xs), y)
+
+
+def lala(year):
+    sierras = gpd.read_file(f"{USER_PATH}/data/shapefiles/sierras.shp")
+    sierras_box = shape_processor.get_box(sierras)
+    return train_rf_rh98(year, sierras_box, True, True)
 
 
 def train_rf(year, geometry, save: bool = True, log: bool = False):
@@ -83,6 +92,69 @@ def train_rf(year, geometry, save: bool = True, log: bool = False):
             save_pickle(f"{DATA_PATH}/rf/models/model_{year}.pkl", m)
             logger.debug(f"Save TabularPandas object for inference.")
             to.export(f"{DATA_PATH}/rf/models/to_{year}.pkl")
+
+    return r2_train, m.oob_score_, r2_test, rmse_train, rmse_test
+
+
+def train_rf_rh98(year, geometry, save: bool = True, log: bool = False):
+    # Get data for this year.
+    logger.debug("Load training data from a pickle file.")
+    gedi = load_pickle(f"{DATA_PATH}/rf/rh98/unburned/gedi_match_{year}.pkl")
+
+    landsat_timeseries_legth = min(5, year - 1984)
+
+    landsat_columns = [f"{x}_{y}"
+                       for y in range(year - landsat_timeseries_legth, year)
+                       for x in gedi_raster_matching.get_landsat_bands(y)]
+    columns_to_use = ['rh_98', 'elevation_median', 'slope_median',
+                      'aspect_median', 'soil_median', 'gedi_year', 'gedi_month'] + landsat_columns
+
+    # Split data into training and validation.
+    logger.debug("Split data into training and testing.")
+    gedi_prepared = split_data.spatial_split_train_and_test_data(
+        gedi, geometry, 5000)
+
+    dep_var = 'rh_98'
+    if log:
+        logger.debug("Optimizing for log")
+        gedi_prepared = gedi_prepared[gedi_prepared.rh_98 != 0]
+        gedi_prepared[dep_var] = np.log(gedi_prepared[dep_var])
+
+    logger.debug("Prepare data for training.")
+    df = gedi_prepared[columns_to_use]
+    procs = [Categorify, FillMissing, Normalize]
+
+    cont, cat = cont_cat_split(df, 1, dep_var=dep_var)
+    splits = IndexSplitter(
+        gedi_prepared[gedi_prepared.dataset == "test"].index)((range_of(df)))
+    to = TabularPandas(df, procs, cat, cont, y_names=dep_var, splits=splits)
+
+    xs, y = to.train.xs, to.train.y
+    valid_xs, valid_y = to.valid.xs, to.valid.y
+
+    logger.debug("Start model training.")
+    m = rf(xs, y)
+    logger.debug("Training complete.")
+
+    rmse_train = m_rmse(m, xs, y)
+    rmse_test = m_rmse(m, valid_xs, valid_y)
+    r2_train = m_r2(m, xs, y)
+    r2_test = m_r2(m, valid_xs, valid_y)
+
+    logger.info(f"Year {year} - Training rmse: {rmse_train}; R^2: {r2_train}")
+    logger.info(f"Year {year} - Validation error: {m.oob_score_}")
+    logger.info(f"Year {year} - Test rmse: {rmse_test}; R^2: {r2_test}")
+
+    if save:
+        logger.debug(f"Save RF model for inference.")
+        if log:
+            save_pickle(f"{DATA_PATH}/rf/rh98/models_log/model_{year}.pkl", m)
+            logger.debug(f"Save TabularPandas object for inference.")
+            to.export(f"{DATA_PATH}/rf/rh98/models_log/to_{year}.pkl")
+        else:
+            save_pickle(f"{DATA_PATH}/rf/rh98/models/model_{year}.pkl", m)
+            logger.debug(f"Save TabularPandas object for inference.")
+            to.export(f"{DATA_PATH}/rf/rh98/models/to_{year}.pkl")
 
     return r2_train, m.oob_score_, r2_test, rmse_train, rmse_test
 
